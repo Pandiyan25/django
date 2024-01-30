335ddef16011364aa031428363b313c77b1c1037
Pandiyan25 <104058263+Pandiyan25@users.noreply.github.com>
2024-01-30 08:00:04 +0530

    first

---
+++ mysql_db/MYSQL_DATABASE/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MysqlDatabaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MYSQL_DATABASE'
---
+++ mysql_db/MYSQL_DATABASE/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  age=models.IntegerField()
+  email=models.EmailField()
+
+  class Meta:
+    db_table='datas'
